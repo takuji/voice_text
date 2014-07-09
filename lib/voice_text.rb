ad9d7529aaa21a3b850ca2840045e0174b7a66e6
Takuji Shimokawa <shimokawa@soba-project.com>
2014-07-09 20:44:25 +0900

    Add text_to_speech method

--- lib/voice_text.rb
+++ lib/voice_text.rb
@@ -5,26 +5,49 @@ require 'tempfile'
 require 'voice_text/version'
 
 class VoiceText
+  DEFAULT_OPTIONS = {
+    speaker: :show,
+    emotion: nil,
+    emotion_level: nil,
+    pitch: 100,
+    speed: 100,
+    volume: 100
+  }
+
+  SPEAKERS = [:show, :haruka, :hikari, :takeru]
+  EMOTIONS = [:happiness, :anger, :sadness]
+
+  module Speakers
+    SHOW   = :show
+    HARUKA = :haruka
+    HIKARI = :hikari
+    TAKERU = :takeru
+  end
+
+  module Emotions
+    HAPINESS = :happiness
+    ANGER    = :anger
+    SADNESS  = :sadness
+  end
+
   def initialize(api_key)
     @api_key = api_key
   end
 
-  def download(options)
-    options[:text] = 'こんにちは' unless options[:text]
-    options[:sperker] = 'show'  unless options[:speaker]
-    # @options = {
-    #   text: options[text] || 'お前はもう死んでいる',
-    #   speaker: options[:speaker] || 'show'
-    #   emotion: options[:emotion],
-    #   emotion_level: options[:emotion_level],
-    #   pitch: options[:pitch] || 100,
-    #   speed: options[:speed] || 100,
-    #   volume: options[:volume] || 100
-    # }
+  def text_to_speech(text, options)
+    params = DEFAULT_OPTIONS.merge(options).select{|k,v| v != nil}
+    params[:text] = text
+    if params[:speaker] == :show && params[:emotion]
+      raise "Cannot specify the emotion parameter for 'show' voice"
+    end
+    download(params)
+  end
+
+  def download(params)
     uri = URI("https://api.voicetext.jp/v1/tts")
     req = Net::HTTP::Post.new(uri.request_uri)
     req.basic_auth @api_key, ''
-    req.set_form_data options
+    req.set_form_data params
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
--- test/voice_text_test.rb
+++ test/voice_text_test.rb
@@ -8,6 +8,21 @@ api_key = ENV['VOICE_TEXT_API_KEY']
 
 vt = VoiceText.new(api_key)
 
-file = vt.download(text: 'おはようございます', speaker: 'hikari')
+FileUtils.mkdir('voices') unless File.exist?('voices')
 
-FileUtils.move(file, "./voice.wav")
+text = '我々は一人の英雄を失った。しかし、これは敗北を意味するのか？否！始まりなのだ！ '
+
+VoiceText::SPEAKERS.each do |speaker|
+  emotions = [nil] + VoiceText::EMOTIONS
+  emotions.each do |emotion|
+    [50, 100, 200].each do |pitch|
+      begin
+        puts "#{text}, speaker:#{speaker}, emotion:#{emotion}, pitch:#{pitch}"
+        file = vt.text_to_speech(text, speaker: speaker, emotion: emotion, pitch: pitch)
+        FileUtils.move(file, "./voices/voice_#{speaker}_#{emotion}_#{pitch}.wav")
+      rescue => e
+        puts e.message
+      end
+    end
+  end
+end
